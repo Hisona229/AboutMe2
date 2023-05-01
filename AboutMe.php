09dabae521af41e2189bbfdb3979417ccc9129fb
Hisona229 <127940984+Hisona229@users.noreply.github.com>
2023-05-01 10:53:27 +0300

    Initial commit

---
+++ AboutMe.php
@@ -0,0 +1,56 @@
+<html>
+
+<head>
+    <title>About me</title>
+    <h1>
+        <e>Трішки інфи про мене</e>
+    </h1>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alkatra&display=swap" rel="stylesheet">
+    <style>
+        p1 {
+            color: darkslategrey;
+            font-size: 25;
+            font-family: 'Alkatra', cursive;
+        }
+
+        e {
+            color: darkslategrey;
+            font-family: 'Alkatra', cursive;
+            font-size: 2.5em;
+        }
+
+        .MyImage {
+            width: 300px;
+        }
+
+        .icon {
+            width: 20px;
+        }
+    </style>
+    <link rel="Styles" href="assets/Styles.css">
+
+</head>
+
+<body>
+
+    <table>
+        <td><img src="image/giga.jpg" class="MyImage"></td>
+        <td>
+            <h3>
+                <p1>Мене звати Артем мені скільки-то років<br>
+                    я навчаюсь в ХНУ і недолюблюю рашиків</p1>
+            </h3>
+            <p>
+            <h3>
+                <p1>link:</p1>
+                <a href="https://www.youtube.com/watch?v=gD7oxlSpisY"><img src="image/youtube.png" class="icon"></a>
+                <a href="https://t.me/Hisonas"><img src="image/telegram.png" class="icon"></a></p>
+                <p><a href="tel:+09657911899">+38 (096) 579-11-89</a></p>
+            </h3>
+        </td>
+    </table>
+</body>
+
+</html>
